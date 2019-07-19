01b491b90cb91f0b6ddf4d4635633067e1a03b3c
mangojuicedeli <mgkang0317@gmail.com>
2019-07-19 14:39:52 +0900

    Add Controller, Service, Mapper, Domain packages and mapper.xml

---
+++ src/main/java/com/bbshop/bit/domain/SavingsVO.java
@@ -0,0 +1,37 @@
+package com.bbshop.bit.domain;
+
+import java.util.Date;
+
+public class SavingsVO {
+
+	Date or_date;
+	String or_items;
+	int or_savings;
+	int or_num;
+	
+	public Date getOr_date() {
+		return or_date;
+	}
+	public void setOr_date(Date or_date) {
+		this.or_date = or_date;
+	}
+	public String getOr_items() {
+		return or_items;
+	}
+	public void setOr_items(String or_items) {
+		this.or_items = or_items;
+	}
+	public int getOr_savings() {
+		return or_savings;
+	}
+	public void setOr_savings(int or_savings) {
+		this.or_savings = or_savings;
+	}
+	public int getOr_num() {
+		return or_num;
+	}
+	public void setOr_num(int or_num) {
+		this.or_num = or_num;
+	}
+	
+}
---
+++ src/main/java/com/bbshop/bit/service/MyPageServiceImpl.java
@@ -0,0 +1,30 @@
+package com.bbshop.bit.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bbshop.bit.domain.SavingsVO;
+import com.bbshop.bit.mapper.MyPageMapper;
+
+@Service("myPageService")
+public class MyPageServiceImpl implements MyPageService {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Override
+	public List<SavingsVO> getSavingsList(int user_key) {
+		
+		MyPageMapper myPageMapper = sqlSession.getMapper(MyPageMapper.class);
+		
+		/* HashMap<String, Integer> map = new HashMap<String, Integer>();
+		map.put("user_key", new Integer(user_key)); */
+				
+		return myPageMapper.getSavingsList(user_key);
+	}
+
+}
