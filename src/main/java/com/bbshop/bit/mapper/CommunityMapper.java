875b61f0a8998ca576e93baac7b8c62477fbdab0
jisoodal <33686837+jisoodal@users.noreply.github.com>
2019-07-25 14:35:28 +0900

    [UPDATE] CommunityMapper.java 변경
    1.  insertPost 반환타입 void에서 int로 변경(제대로 insert 되었는지 확인하기 위함)
    2. getNickname 메서드 추가(글 작성 페이지에서 닉네임을 value에 고정시켜 놓기 위함)

--- src/main/java/com/bbshop/bit/mapper/CommunityMapper.java
+++ src/main/java/com/bbshop/bit/mapper/CommunityMapper.java
@@ -1,9 +1,11 @@
 package com.bbshop.bit.mapper;
 
 import com.bbshop.bit.domain.CommunityVO;
+import com.bbshop.bit.domain.MemberVO;
 
 public interface CommunityMapper {
 	
-	public void insertPost(CommunityVO community);
+	public int insertPost(CommunityVO community);
+	public String getNickname(int user_key);
 
 }
