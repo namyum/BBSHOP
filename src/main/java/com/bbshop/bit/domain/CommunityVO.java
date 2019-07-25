25b52c7e792e0176c7c7c3cf3a16607e171ec305
jisoodal <33686837+jisoodal@users.noreply.github.com>
2019-07-25 18:14:43 +0900

    [DELETE] CommunityVO에서 ip 컬럼 삭제

--- src/main/java/com/bbshop/bit/domain/CommunityVO.java
+++ src/main/java/com/bbshop/bit/domain/CommunityVO.java
@@ -7,24 +7,24 @@ import lombok.Data;
 @Data
 public class CommunityVO {
 	
-	private Long board_num;	// �� ��ȣ
-	private String team_name; // ���ܸ�(���Ӹ��� �� �κ�)
-	private String title;	// �� ����
-	private String board_content;	// �� ����
-	private String uploadFile;	// ÷������
-//	private String writer;	// ȸ��_ȸ���ĺ���.�г������� �ҷ����� �ɵ�!
-	private Long hit; // �� ��ȸ��, default 0
-	private Date regdate;	// �� �ۼ���, default sysdate
-	private Date moddate;	// �� ������, default sysdate
-	private Long previousPost;	// ������
-	private Long nextPost;	// ������
-	private Long reply_num;	// ��� ����
-	private String uri;	// �� uri(�Ű��� �� �Ѱ��ֱ� ����)
-	private Long report_num;	// �Ű� Ƚ��
-	private String ip;	// ip �ּ�
-	private Long user_key;	// ȸ�� �ĺ���
-	private Long seq;	// �� ������ȣ
-	private Long lev;	// ��� ����
-	private Long dep;	// ��� ����
-	private int seq_board;
+	private Long BOARD_NUM;
+	private String TEAM_NAME;
+	private String TITLE;
+	private String BOARD_CONTENT;
+	private String UPLOADFILE;
+	private String WRITER;	
+	private Long HIT; 
+	private Date REGDATE;
+	private Date MODDATE;	
+	private Long PREVIOUSPOST;	
+	private Long NEXTPOST;
+	private Long REPLY_NUM;	
+	private String URI;	
+	private Long REPORT_NUM;	
+//	private String IP;	
+	private Long USER_KEY;	
+	private Long SEQ;	
+	private Long LEV;	
+	private Long DEP;	
+	private int SEQ_BOARD;
 }
