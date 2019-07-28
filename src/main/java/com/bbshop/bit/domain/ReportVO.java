abda77664a680bfce100ebe9b3c36e8211fd74b4
jisooLee <angeljess@naver.com>
2019-07-29 01:46:30 +0900

    [ADD] ReportVO 생성

---
+++ src/main/java/com/bbshop/bit/domain/ReportVO.java
@@ -0,0 +1,13 @@
+package com.bbshop.bit.domain;
+
+import lombok.Data;
+
+@Data
+public class ReportVO {
+	
+	private Long REPORT_NUM;
+	private String RE_CATEGORY;
+	private Long BOARD_NUM;
+	private Long REPLY_NUM;
+
+}
