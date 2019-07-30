58fd1769db98c693655c450cc0006fadf4556abf
mangojuicedeli <mgkang0317@gmail.com>
2019-07-31 01:21:15 +0900

    세션에서 받은 id로 회원/비회원 구별 구현

--- src/main/java/com/bbshop/bit/controller/GoodsController.java
+++ src/main/java/com/bbshop/bit/controller/GoodsController.java
@@ -2,7 +2,10 @@ package com.bbshop.bit.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,18 +26,26 @@ import lombok.extern.log4j.Log4j;
 @AllArgsConstructor
 public class GoodsController {
 	
+	@Autowired
 	private GoodsService service;
 	
+	private HttpSession session; // 로그인 시에 session에 id값이 담겨있다.
 	
 	// 상품 목록 페이지
 	@RequestMapping(value="/goods_list.do", method=RequestMethod.GET)
-	public String goods_list(@RequestParam int category, PagingVO pagingVO, Model model) {
+	public String goods_list(@RequestParam(required=false, defaultValue="1") int category, PagingVO pagingVO, Model model) {
+		
 		log.info("Controller...goods_list.jsp");
+		
+		String id = (String)session.getAttribute("id");
+		
+		// id = "noAccount";
 		
 		model.addAttribute("categoryInt", category);
 		model.addAttribute("categoryString", service.category(category));
 		
 		model.addAttribute("pageMaker", new PageDTO(pagingVO, 123));
+		model.addAttribute("id", id); // 세션에서 받은 id 값을 model에 추가함.
 		
 		return "shoppingMall/goods/goods_list";
 	}
