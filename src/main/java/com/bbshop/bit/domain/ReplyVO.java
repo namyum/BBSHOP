b619013b6ba9036587f15b9521628cbadf04fa20
jisoodal <33686837+jisoodal@users.noreply.github.com>
2019-07-30 15:33:20 +0900

    [UPDATE] ReplyVO 변수 소문자로 변경
    Rest 구현 중 대문자로 VO를 생성하면 오류 발생함에 따라 소문자로 변경함

--- src/main/java/com/bbshop/bit/domain/ReplyVO.java
+++ src/main/java/com/bbshop/bit/domain/ReplyVO.java
@@ -2,18 +2,23 @@ package com.bbshop.bit.domain;
 
 import java.util.Date;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReplyVO {
 	
-	private Long REPLY_NUM;
-	private String REPLY_CONTENT;
-	private Date REGDATE;
-	private Date MODDATE;
-	private Long REPORT_NUM;
-	private Long BOARD_NUM;
-	private Long USER_KEY;
-	private String WRITER;
+	private Long reply_num;
+	private String reply_content;
+	private Date regdate;
+	private Date moddate;
+	private Long report_num;
+	private Long board_num;
+	private Long user_key;
+	private String writer;
+	private int seq_reply;
 
 }
