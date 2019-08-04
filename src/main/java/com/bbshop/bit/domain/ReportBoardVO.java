8b522096ee31d47cfa43fb9ee28b04d18333a0e6
jisoodal <33686837+jisoodal@users.noreply.github.com>
2019-08-04 16:15:23 +0900

    [ADD] 댓글 신고와 게시글 신고 VO 따로 생성

---
+++ src/main/java/com/bbshop/bit/domain/ReportBoardVO.java
@@ -0,0 +1,15 @@
+package com.bbshop.bit.domain;
+
+import lombok.Data;
+
+@Data
+public class ReportBoardVO {
+	
+	private Long REPORT_NUM;
+	private String RE_CATEGORY;
+	private Long BOARD_NUM;
+	private String WRITER;
+	private String URI;
+	private int SEQ_REPORTBOARD;
+
+}
